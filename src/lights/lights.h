6443c257740d4ecede4d26813033b2353aec1b95
Zephyr308 <87972042+Zephyr308@users.noreply.github.com>
2026-01-20 16:31:58 +0300

    Rename light_set to LIGHTS_Set and update guards

--- src/lights/lights.h
+++ src/lights/lights.h
@@ -1,11 +1,13 @@
-#ifndef LIGHT_H__
-#define LIGHT_H__
+#ifndef LIGHTS_H
+#define LIGHTS_H
+
+#include <stdint.h>
 
 #define RED   (1U<<1)
 #define BLUE  (1U<<2)
 #define GREEN (1U<<3)
 
 void LIGHTS_Init(void);
-void light_set(uint8_t color);
+void LIGHTS_Set(uint8_t mask);
 
 #endif
