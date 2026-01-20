16c251e298d853edc5e49e6d64187d4a667602ff
Zephyr308 <87972042+Zephyr308@users.noreply.github.com>
2026-01-20 16:57:00 +0300

    Update timing.c

--- src/timing/timing.c
+++ src/timing/timing.c
@@ -1,37 +1,17 @@
 #include "TM4C123GH6PM.h"
 #include "timing.h"
 
-volatile bool task_sense;
-volatile bool task_control;
-volatile bool task_behavior;
-
 void TIMING_Init(void)
 {
-    SYSCTL->RCGCTIMER |= (1<<2);
-    TIMER2->CTL = 0;
-    TIMER2->CFG = 0x04;
-    TIMER2->TAMR = 0x02;
-    TIMER2->TAILR = 16000; // 1ms @16MHz
-    TIMER2->IMR |= 1;
-    TIMER2->CTL |= 1;
-    NVIC->ISER[0] |= (1<<23);
+    SYSCTL->RCGCTIMER |= (1U << 1);   // Timer1
+    TIMER1->CTL = 0;
+    TIMER1->CFG = 0x0;
+    TIMER1->TAMR = 0x02;              // periodic
+    TIMER1->TAILR = 0xFFFFFFFF;       // free-running
+    TIMER1->CTL |= 0x01;
 }
 
-void TIMING_Tick(void)
+uint32_t TIMING_GetTicksUs(void)
 {
-    static uint8_t div = 0;
-
-    task_sense = true;
-    task_control = true;
-
-    if (++div >= 10) {
-        task_behavior = true;
-        div = 0;
-    }
+    return TIMER1->TAR;
 }
-
-uint32_t TIMING_GetTicks(void)
-{
-    return TIMER1->TAR; // free-running down-counter
-}
-
