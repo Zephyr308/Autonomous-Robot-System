f3b717941da2c54b68942c5a6c20f8075256a502
Zephyr308 <87972042+Zephyr308@users.noreply.github.com>
2026-01-20 16:56:21 +0300

    Refactor timing.h to use uint32_t for ticks
    Removed boolean task variables and updated function signatures.

--- src/timing/timing.h
+++ src/timing/timing.h
@@ -1,15 +1,9 @@
 #ifndef TIMING_H
 #define TIMING_H
 
-#include <stdbool.h>
-
-extern volatile bool task_sense;
-extern volatile bool task_control;
-extern volatile bool task_behavior;
+#include <stdint.h>
 
 void TIMING_Init(void);
-void TIMING_Tick(void);
-uint32_t TIMING_GetTicks(void);   // returns microseconds
-
+uint32_t TIMING_GetTicksUs(void);
 
 #endif
