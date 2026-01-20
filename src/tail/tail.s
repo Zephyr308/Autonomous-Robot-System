f69e577c1755487c7a6eec5f7b344bee2436aa76
Zephyr308 <87972042+Zephyr308@users.noreply.github.com>
2026-01-20 16:30:26 +0300

    Implement TAIL_Init and TAIL_Read in assembly
    Added assembly code for TAIL_Init and TAIL_Read functions to configure GPIO.

--- src/tail/tail.s
+++ src/tail/tail.s
@@ -1,2 +1,33 @@
-; Assembly driver unchanged; sets PA5 as input
-; Provides TAIL_Init and TAIL_sense functions
+GPIO_PORTA_DIR_R   EQU 0x40004400
+GPIO_PORTA_DEN_R   EQU 0x4000451C
+SYSCTL_RCGCGPIO_R  EQU 0x400FE608
+GPIO_PORTA_DATA_R  EQU 0x400043FC
+
+        AREA |.text|, CODE, READONLY
+        THUMB
+        EXPORT TAIL_Init
+        EXPORT TAIL_Read
+
+TAIL_Init
+        LDR R1, =SYSCTL_RCGCGPIO_R
+        LDR R0, [R1]
+        ORR R0, R0, #1
+        STR R0, [R1]
+
+        LDR R1, =GPIO_PORTA_DIR_R
+        LDR R0, [R1]
+        BIC R0, R0, #0x20
+        STR R0, [R1]
+
+        LDR R1, =GPIO_PORTA_DEN_R
+        LDR R0, [R1]
+        ORR R0, R0, #0x20
+        STR R0, [R1]
+        BX LR
+
+TAIL_Read
+        LDR R1, =GPIO_PORTA_DATA_R
+        LDR R0, [R1]
+        AND R0, R0, #0x20
+        BX LR
+        END
