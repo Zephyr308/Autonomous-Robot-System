9b6e02c2f282a7ebf94d53a176d50bac8ccd5777
Zephyr308 <87972042+Zephyr308@users.noreply.github.com>
2026-01-20 16:37:58 +0300

    Refactor behavior.h with new enum and function name
    Refactor behavior state enumeration and update function naming.

--- src/behaviour/behavior.h
+++ src/behaviour/behavior.h
@@ -1,6 +1,7 @@
-#ifndef BEHAVIOR_H__
-#define BEHAVIOR_H__
+typedef enum {
+    MOVE,
+    AVOID_LEFT,
+    AVOID_RIGHT
+} behavior_state_t;
 
-void behavior_update(void);
-
-#endif
+void BEHAVIOR_Update(void);
