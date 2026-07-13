6e2c7025d8f1a6cc08591b4b8cbdc629577d0bb9
Zephyr308 <87972042+Zephyr308@users.noreply.github.com>
2026-07-13 22:37:21 +0300

    Refactor SysTick initialization and task scheduler

--- src/timing/timing.c
+++ src/timing/timing.c
@@ -1,17 +1,242 @@
+/**
+ * @file timing.c
+ * @brief Cooperative task scheduler.
+ */
+
+
 #include "TM4C123GH6PM.h"
+
 #include "timing.h"
+
+
+
+/*
+ * Maximum firmware tasks.
+ *
+ * Increase if required.
+ */
+
+#define MAX_TASKS     12U
+
+
+
+/*
+ * Scheduler task list
+ */
+
+static SchedulerTask task_table[MAX_TASKS];
+
+
+
+static uint8_t task_count;
+
+
+
+/*
+ * System time counter.
+ *
+ * Updated by SysTick interrupt.
+ */
+
+static volatile uint32_t system_time;
+
+
+
+/*
+ *---------------------------------------------------------
+ * SysTick initialization
+ *---------------------------------------------------------
+ */
 
 void TIMING_Init(void)
 {
-    SYSCTL->RCGCTIMER |= (1U << 1);   // Timer1
-    TIMER1->CTL = 0;
-    TIMER1->CFG = 0x0;
-    TIMER1->TAMR = 0x02;              // periodic
-    TIMER1->TAILR = 0xFFFFFFFF;       // free-running
-    TIMER1->CTL |= 0x01;
+
+    system_time = 0;
+
+
+    task_count = 0;
+
+
+
+    /*
+     * 16MHz clock
+     *
+     * 10ms tick:
+     *
+     * 16,000,000 * 0.01
+     *
+     * =160000 cycles
+     */
+
+    SysTick->LOAD =
+        160000U - 1U;
+
+
+
+    SysTick->VAL = 0;
+
+
+
+    /*
+     * Enable interrupt
+     *
+     * Use processor clock
+     */
+
+    SysTick->CTRL =
+        (1U<<2) |
+        (1U<<1);
+
 }
 
-uint32_t TIMING_GetTicksUs(void)
+
+
+/*
+ *---------------------------------------------------------
+ * Start scheduler clock
+ *---------------------------------------------------------
+ */
+
+void TIMING_Start(void)
 {
-    return TIMER1->TAR;
+
+    SysTick->CTRL |= 1U;
+
+}
+
+
+
+/*
+ *---------------------------------------------------------
+ * SysTick interrupt
+ *---------------------------------------------------------
+ */
+
+void SysTick_Handler(void)
+{
+
+    system_time += SYSTEM_TICK_MS;
+
+}
+
+
+
+/*
+ *---------------------------------------------------------
+ * Current system time
+ *---------------------------------------------------------
+ */
+
+uint32_t TIMING_GetMillis(void)
+{
+
+    return system_time;
+
+}
+
+
+
+/*
+ *---------------------------------------------------------
+ * Add task
+ *---------------------------------------------------------
+ */
+
+uint8_t TIMING_AddTask
+(
+    TaskFunction function,
+    uint32_t period_ms
+)
+{
+
+    if(task_count >= MAX_TASKS)
+    {
+        return 0;
+    }
+
+
+
+    if(function == 0)
+    {
+        return 0;
+    }
+
+
+
+    task_table[task_count].function =
+        function;
+
+
+    task_table[task_count].period_ms =
+        period_ms;
+
+
+    task_table[task_count].last_run =
+        0;
+
+
+    task_table[task_count].enabled =
+        1;
+
+
+
+    task_count++;
+
+
+    return 1;
+
+}
+
+
+
+/*
+ *---------------------------------------------------------
+ * Run scheduler
+ *---------------------------------------------------------
+ */
+
+void TIMING_Run(void)
+{
+
+    uint8_t i;
+
+
+    uint32_t now;
+
+
+    now = system_time;
+
+
+
+    for(i=0;i<task_count;i++)
+    {
+
+
+        if(task_table[i].enabled == 0)
+        {
+            continue;
+        }
+
+
+
+        if((now - task_table[i].last_run)
+            >= task_table[i].period_ms)
+        {
+
+            /*
+             * Update timestamp before running.
+             *
+             * Prevents timing drift.
+             */
+
+            task_table[i].last_run = now;
+
+
+
+            task_table[i].function();
+
+        }
+
+    }
+
 }
